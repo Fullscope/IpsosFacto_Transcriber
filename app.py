b6a844a5dac618f6921cb3d28d38069ae108b2a5
Alex Tamvakis <alex.tamvakis@gmail.com>
2024-02-06 11:55:03 -0600

    Init

---
+++ app.py
@@ -0,0 +1,12 @@
+# Import the required libraries
+import streamlit as st
+
+# Create the main function
+def main():
+    # Add your Streamlit code here
+    st.title("My Streamlit Project")
+    st.write("Welcome to my project!")
+
+# Run the main function
+if __name__ == '__main__':
+    main()
